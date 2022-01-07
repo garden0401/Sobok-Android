defa809303ea1e8aff4a3ca8501075e4d2583fa2
onpyeong <gsh447114@gmail.com>
2022-01-08 02:16:55 +0900

    [CHORE] #1 - ActionBar 삭제

---
+++ app/src/main/java/com/example/sobok_android/presentation/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sobok_android.presentation.view
+
+import android.os.Bundle
+import com.example.sobok_android.R
+import com.example.sobok_android.databinding.ActivityMainBinding
+import com.example.sobok_android.presentation.base.BindingActivity
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
