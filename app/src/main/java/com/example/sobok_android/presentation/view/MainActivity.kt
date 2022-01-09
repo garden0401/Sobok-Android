0d5a77eb5842eb5a79256399227583996332fbeb
onpyeong <gsh447114@gmail.com>
2022-01-10 04:27:48 +0900

    [FEAT] #5 - BottomNavigation 연결

--- app/src/main/java/com/example/sobok_android/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.sobok_android.presentation.view
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.sobok_android.R
 import com.example.sobok_android.databinding.ActivityMainBinding
 import com.example.sobok_android.presentation.base.BindingActivity
 
+
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setNavigation()
+    }
 
+    private fun setNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bnvMain.setupWithNavController(navController)
     }
 }
