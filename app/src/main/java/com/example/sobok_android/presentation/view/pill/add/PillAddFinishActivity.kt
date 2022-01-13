9e350e260d9991eaadedb166a919fdf13c9225bd
chaeyeong <shimcy2010@gmail.com>
2022-01-13 12:43:19 +0900

    [UI] #6 - 약추가-원페이지, 내약목록 UI 엑티비티로 변경

---
+++ app/src/main/java/com/example/sobok_android/presentation/view/pill/add/PillAddFinishActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sobok_android.presentation.view.pill.add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PillAddFinishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pill_add_finish2)
+    }
+}
