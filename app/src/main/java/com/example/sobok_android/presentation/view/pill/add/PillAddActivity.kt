b7d7451f41a8c5292164cb4826dc173b5704cce6
chaeyeong <shimcy2010@gmail.com>
2022-01-13 16:03:59 +0900

    [UI] #6 - 약추가-원페이지 : 리사이클러뷰 추가

--- app/src/main/java/com/example/sobok_android/presentation/view/pill/add/PillAddActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/pill/add/PillAddActivity.kt
@@ -2,8 +2,11 @@ package com.example.sobok_android.presentation.view.pill.add
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sobok_android.R
+import com.example.sobok_android.databinding.ActivityPillAddBinding
+import com.example.sobok_android.presentation.base.BindingActivity
 
-class PillAddActivity : AppCompatActivity() {
+class PillAddActivity : BindingActivity<ActivityPillAddBinding>(R.layout.activity_pill_add) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
--- app/src/main/java/com/example/sobok_android/presentation/view/pill/add/PillAddFinishActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/pill/add/PillAddFinishActivity.kt
@@ -2,10 +2,11 @@ package com.example.sobok_android.presentation.view.pill.add
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sobok_android.R
 
 class PillAddFinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pill_add_finish2)
+        setContentView(R.layout.activity_pill_add_finish)
     }
 }
