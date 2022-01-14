7d5f920bcc6b12d6dceda85622800dda23036f5d
chaeyeong <shimcy2010@gmail.com>
2022-01-14 11:55:56 +0900

    [UI] 약추가 UI 리팩터링

--- app/src/main/java/com/example/sobok_android/presentation/view/pill/add/PillAddFinishActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/pill/add/PillAddFinishActivity.kt
@@ -3,8 +3,11 @@ package com.example.sobok_android.presentation.view.pill.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.sobok_android.R
+import com.example.sobok_android.databinding.ActivityPillAddBinding
+import com.example.sobok_android.presentation.base.BindingActivity
+import com.example.sobok_android.presentation.base.BindingFragment
 
-class PillAddFinishActivity : AppCompatActivity() {
+class PillAddFinishActivity : BindingActivity<ActivityPillAddBinding>(R.layout.activity_pill_add_finish) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pill_add_finish)
