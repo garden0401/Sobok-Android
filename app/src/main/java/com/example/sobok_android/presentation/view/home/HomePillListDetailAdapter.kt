26a9f6beae57580e024608d98da708c3782f64c8
garden <0401ah@gmail.com>
2022-01-17 08:53:28 +0900

    [MODIFY] #10-약 리스트 체크 & 체크 취소 코드 추가

--- app/src/main/java/com/example/sobok_android/presentation/view/home/HomePillListDetailAdapter.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/home/HomePillListDetailAdapter.kt
@@ -3,23 +3,25 @@ package com.example.sobok_android.presentation.view.home
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sobok_android.R
 import com.example.sobok_android.databinding.ItemHomePillListDetailBinding
 
 
-class HomePillListDetailAdapter : RecyclerView.Adapter<HomePillListDetailAdapter.HomePillListDetailViewHolder>() {
+class HomePillListDetailAdapter :
+    RecyclerView.Adapter<HomePillListDetailAdapter.HomePillListDetailViewHolder>() {
 
     private val _homePillListDetail = mutableListOf<HomePillListData.PillDetailData>()
 
-    var homePillListDetail: List<HomePillListData.PillDetailData> =  _homePillListDetail
+    var homePillListDetail: List<HomePillListData.PillDetailData> = _homePillListDetail
         set(value) {
             _homePillListDetail.clear()
             _homePillListDetail.addAll(value)
             notifyDataSetChanged()
         }
 
-    private var stickerClickListener : ((Boolean) -> Unit)? = null
+    private var stickerClickListener: ((Boolean) -> Unit)? = null
 
     fun setStickerClickListener(listener: (Boolean) -> Unit) {
         stickerClickListener = listener
@@ -33,6 +35,7 @@ class HomePillListDetailAdapter : RecyclerView.Adapter<HomePillListDetailAdapter
             LayoutInflater.from(parent.context),
             parent, false
         )
+
         return HomePillListDetailViewHolder(binding)
     }
 
@@ -42,30 +45,35 @@ class HomePillListDetailAdapter : RecyclerView.Adapter<HomePillListDetailAdapter
 
     override fun getItemCount(): Int = _homePillListDetail.size
 
-    inner class HomePillListDetailViewHolder(private val binding : ItemHomePillListDetailBinding)
-        : RecyclerView.ViewHolder(binding.root) {
-            fun onBind(data : HomePillListData.PillDetailData) {
-                binding.tvHomePillListPillName.text = data.pillName
-                binding.ivHomePillListCheck.isSelected = data.isCheck
-                binding.ivHomePillListStickerOne.setImageResource(R.drawable.rectangle_pill_list_sticker_example)
-                binding.ivHomePillListColor.setImageResource(R.drawable.oval_pill_list_color)
-                binding.tvHomePillListStickerCount.text = data.stickerCount
+    inner class HomePillListDetailViewHolder(private val binding: ItemHomePillListDetailBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: HomePillListData.PillDetailData) {
+            binding.tvHomePillListPillName.text = data.pillName
+            binding.ivHomePillListCheck.isSelected = data.isCheck
+            binding.ivHomePillListStickerOne.setImageResource(R.drawable.rectangle_pill_list_sticker_example)
+            binding.ivHomePillListColor.setImageResource(R.drawable.oval_pill_list_color)
+            binding.tvHomePillListStickerCount.text = data.stickerCount
 
-
-
-                    binding.ivHomePillListStickerOne.setOnClickListener {
-                        stickerClickListener?.invoke(true)
-                        Log.d("detailAdapter", "true")
-                    }
-
-                binding.tvHomePillListPillName.setOnClickListener {
-                    stickerClickListener?.invoke(true)
-                    Log.d("tvtv", "click")
-                }
-
-
-
-
-                }
+            // 약 리스트 체크 & 체크 취소
+            binding.ivHomePillListCheck.setOnClickListener {
+                binding.ivHomePillListCheck.isSelected = !binding.ivHomePillListCheck.isSelected
             }
+
+
+            binding.ivHomePillListStickerOne.setOnClickListener {
+                stickerClickListener?.invoke(true)
+                Log.d("detailAdapter", "true")
+            }
+
+            binding.tvHomePillListPillName.setOnClickListener {
+                stickerClickListener?.invoke(true)
+                Log.d("tvtv", "click")
+            }
+
+            // 스피너 어댑터 설정
+            // binding.spHomePillList.adapter = ArrayAdapter.createFromResource(this, R.array.spinnerList, android.R.layout.simple_spinner_item)
+
+
+        }
     }
+}
