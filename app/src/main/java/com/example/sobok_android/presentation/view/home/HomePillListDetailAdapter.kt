e4160e22dd902372d81780ee96a1021fc195fbff
garden <0401ah@gmail.com>
2022-01-18 03:28:53 +0900

    [FEAT] close #15 - 홈(메인) 약 체크 <-> 취소 동작

--- app/src/main/java/com/example/sobok_android/presentation/view/home/HomePillListDetailAdapter.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/home/HomePillListDetailAdapter.kt
@@ -73,9 +73,27 @@ class HomePillListDetailAdapter :
 
 
             // 약 리스트 체크 & 체크 취소
-            binding.ivHomePillListCheck.setOnClickListener {
-                binding.ivHomePillListCheck.isSelected = !binding.ivHomePillListCheck.isSelected
+            val check = binding.ivHomePillListCheck
+            check.setOnClickListener {
+            when (check.isSelected) {
+                true -> {
+                    val builder = AlertDialog.Builder(it.context)
+                    builder.setTitle("복약하지 않은 약인가요?")
+                        .setMessage("복약을 취소하면 소중한 사람들의 응원도 같이 삭제되어요.")
+                        .setPositiveButton("복약 취소") { dialog, id ->
+                            // 복약 취소 행
+                        }
+                        .setNegativeButton("취소"
+                        ) { dialog, id ->
+                            dialog.cancel()
+                        }
+                    builder.show()
+
+                }
+                else -> {check.isSelected = true}
             }
+            }
+
 
             //약 리스트 수정 클릭시 팝업
             binding.ivHomePillListEdit.setOnClickListener {view->
@@ -94,13 +112,13 @@ class HomePillListDetailAdapter :
                             val builder = AlertDialog.Builder(view.context)
                             builder.setTitle("정말로 복약을 중단하나요?")
                                 .setMessage("복약을 중단하면 내일부터 약 알림이 오지 않아요.")
-                                .setPositiveButton("취소") { dialog, id ->
+                                .setPositiveButton("복약 중단") { dialog, id ->
                                     //클릭리스너
-                                    dialog.cancel()
+
                                 }
-                                .setNegativeButton("복약 중단"
+                                .setNegativeButton("취소"
                                 ) { dialog, id ->
-                                    //클릭리스너
+                                    dialog.cancel()
                                 }
                             builder.show()
 
@@ -111,13 +129,13 @@ class HomePillListDetailAdapter :
                             val builder = AlertDialog.Builder(view.context)
                             builder.setTitle("정말로 약을 삭제하나요?")
                                 .setMessage("해당 약에 대한 전체 복약 기록이 사라지고 알림도 오지 않아요.")
-                                .setPositiveButton("취소") { dialog, id ->
+                                .setPositiveButton("삭제") { dialog, id ->
                                     //클릭리스너
-                                    dialog.cancel()
+
                                 }
-                                .setNegativeButton("삭제"
+                                .setNegativeButton("취소"
                                 ) { dialog, id ->
-                                    //클릭리스너
+                                    dialog.cancel()
                                 }
                             builder.show()
 
