7d387d4c9d22227ae8928e9c266855da91e240b2
onpyeong <gsh447114@gmail.com>
2022-01-18 06:57:05 +0900

    [MODIFY] #31 - 의존성 주입

--- app/src/main/java/com/example/sobok_android/presentation/di/DataSourceModule.kt
+++ app/src/main/java/com/example/sobok_android/presentation/di/DataSourceModule.kt
@@ -2,8 +2,11 @@ package com.example.sobok_android.presentation.di
 
 import com.example.sobok_android.data.datasource.pill.pilladd.PillAddDataSource
 import com.example.sobok_android.data.datasource.pill.pilladd.PillAddLocalDataSource
+import com.example.sobok_android.data.datasource.share.request.ShareRequestDataSource
+import com.example.sobok_android.data.datasource.share.request.ShareRequestLocalDataSource
 import org.koin.dsl.module
 
 val dataSourceModule = module {
     single<PillAddDataSource> { PillAddLocalDataSource() }
+    single<ShareRequestDataSource> { ShareRequestLocalDataSource() }
 }
--- app/src/main/java/com/example/sobok_android/presentation/di/ViewModelModule.kt
+++ app/src/main/java/com/example/sobok_android/presentation/di/ViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.example.sobok_android.presentation.di
 
 import com.example.sobok_android.presentation.view.pill.add.viewmodel.PillAddViewModel
+import com.example.sobok_android.presentation.view.share.request.viewmodel.ShareRequestViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { PillAddViewModel(get())}
+    viewModel { ShareRequestViewModel(get()) }
 }
