31f52ec3d9b5b7243cae689c7290aeb026af6b19
garden <0401ah@gmail.com>
2022-01-17 20:54:09 +0900

    [FEAT] #10 -  MainActivity에 임시버튼 만들어서 popup 띄우기 테스트

--- app/src/main/java/com/example/sobok_android/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.sobok_android.presentation.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.PopupMenu
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -17,13 +20,14 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     private val mainViewModel: MainViewModel by viewModel()
-    private lateinit var homeStickerBottomSheet : HomeStickerBottomSheetFragment
+    private lateinit var homeStickerBottomSheet: HomeStickerBottomSheetFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initHomeStickerBottomSheet()
         setNavigation()
         observeIsStickerClickEvent()
+        setPop()
 
     }
 
@@ -35,7 +39,8 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     }
 
     private fun setNavigation() {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
         val navController = navHostFragment.findNavController()
         binding.bnvMain.setupWithNavController(navController)
     }
@@ -43,12 +48,39 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
     private fun observeIsStickerClickEvent() {
         mainViewModel.isStickerClick.observe(this) {
             Log.d("checkobserve", "${it}")
-            if(it){
+            if (it) {
                 homeStickerBottomSheet.show(supportFragmentManager, homeStickerBottomSheet.tag)
 
             }
         }
 
     }
+
+    // 약 리스트 수정 클릭시 팝업(임시로 홈프래그먼트에 띄워보기)
+    private fun setPop() {
+        binding.btnPoor.setOnClickListener {
+            var popup = PopupMenu(this, binding.btnPoor)
+            menuInflater?.inflate(R.menu.popup_home_pill_list_edit, popup.menu)
+
+            popup.setOnMenuItemClickListener {
+                when (it?.itemId) {
+                    R.id.pill_edit -> {
+                        // 다이얼로그
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.pill_delete -> {
+                        return@setOnMenuItemClickListener true
+                    }
+                    R.id.pill_stop -> {
+                        return@setOnMenuItemClickListener true
+
+                    }
+                }
+                return@setOnMenuItemClickListener false
+            }
+            popup.show()
+        }
+    }
+
 
 }
