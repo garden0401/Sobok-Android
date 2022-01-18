03991c5ada70850435168328cd65100c290a003f
onpyeong <gsh447114@gmail.com>
2022-01-19 08:11:39 +0900

    [MODIFY] #31 - ShareRequestRepository, ShareRequestMapper에 post 추가

--- app/src/main/java/com/example/sobok_android/data/mapper/ShareRequestMapper.kt
+++ app/src/main/java/com/example/sobok_android/data/mapper/ShareRequestMapper.kt
@@ -1,12 +1,20 @@
 package com.example.sobok_android.data.mapper
 
 import com.example.sobok_android.data.model.response.share.request.ResSearchResultData
+import com.example.sobok_android.data.model.response.share.request.ResShareRequestSuccessData
 import com.example.sobok_android.domain.model.share.request.SearchResultData
+import com.example.sobok_android.domain.model.share.request.ShareRequestSuccessData
 
 object ShareRequestMapper {
     fun mapperToSearchResultData(resSearchResultData: ResSearchResultData): SearchResultData {
         return SearchResultData(
             data = resSearchResultData.data.map { SearchResultData.Member(it.memberId, it.memberName) }
+        )
+    }
+
+    fun mapperToSearchResultSuccessData(resShareRequestSuccessData: ResShareRequestSuccessData): ShareRequestSuccessData {
+        return ShareRequestSuccessData(
+            data = ShareRequestSuccessData.MemberInfo(resShareRequestSuccessData.data.memberId, resShareRequestSuccessData.data.memberName)
         )
     }
 
--- app/src/main/java/com/example/sobok_android/data/repository/share/request/ShareRequestRepositoryImpl.kt
+++ app/src/main/java/com/example/sobok_android/data/repository/share/request/ShareRequestRepositoryImpl.kt
@@ -3,10 +3,15 @@ package com.example.sobok_android.data.repository.share.request
 import com.example.sobok_android.data.datasource.share.request.ShareRequestDataSource
 import com.example.sobok_android.data.mapper.ShareRequestMapper
 import com.example.sobok_android.domain.model.share.request.SearchResultData
+import com.example.sobok_android.domain.model.share.request.ShareRequestSuccessData
 import com.example.sobok_android.domain.repository.share.request.ShareRequestRepository
 
 class ShareRequestRepositoryImpl(private val shareRequestDataSource: ShareRequestDataSource) : ShareRequestRepository {
     override suspend fun getSearchResult(userName: String): SearchResultData {
-        return ShareRequestMapper.mapperToSearchResultData(shareRequestDataSource.getSearchResult())
+        return ShareRequestMapper.mapperToSearchResultData(shareRequestDataSource.getSearchResult(userName))
+    }
+
+    override suspend fun postSearchResult(memberId: Int, memberName: String): ShareRequestSuccessData {
+        return ShareRequestMapper.mapperToSearchResultSuccessData(shareRequestDataSource.postSearchResult(memberId, memberName))
     }
 }
