b0015c3d0f2a57b7d6270b575b8f14fff176504f
onpyeong <gsh447114@gmail.com>
2022-01-19 08:12:39 +0900

    [FEAT] #31 - ShareRequest 화면 전환 구현

--- app/src/main/java/com/example/sobok_android/presentation/view/share/request/ShareRequestActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/share/request/ShareRequestActivity.kt
@@ -4,11 +4,51 @@ import android.os.Bundle
 import com.example.sobok_android.R
 import com.example.sobok_android.databinding.ActivityShareRequestBinding
 import com.example.sobok_android.presentation.base.BindingActivity
+import com.example.sobok_android.presentation.view.share.request.viewmodel.ShareRequestViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ShareRequestActivity : BindingActivity<ActivityShareRequestBinding>(R.layout.activity_share_request) {
+class ShareRequestActivity :
+    BindingActivity<ActivityShareRequestBinding>(R.layout.activity_share_request) {
+    private val shareRequestViewModel: ShareRequestViewModel by viewModel()
+    private lateinit var shareRequestSearchFragment: ShareRequestSearchFragment
+    private lateinit var shareRequestSaveFragment: ShareRequestSaveFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initTransactionEvent()
+        observeIsShareRequestSearch()
+    }
+
+    private fun initTransactionEvent() {
+        shareRequestSearchFragment = ShareRequestSearchFragment()
+        shareRequestSaveFragment = ShareRequestSaveFragment()
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fcv_share_request, shareRequestSearchFragment).commit()
+
+        binding.ivShareRequestBack.setOnClickListener {
+            replaceSearchFragment()
+        }
+
+        binding.ivShareRequestClose.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun observeIsShareRequestSearch() {
+        shareRequestViewModel.isShareRequestSearch.observe(this) {
+            binding.isShareRequestSearch = it
+        }
+    }
+
+    fun replaceSaveFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_share_request, shareRequestSaveFragment).commit()
+    }
+
+    fun replaceSearchFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_share_request, shareRequestSearchFragment).commit()
     }
 }
