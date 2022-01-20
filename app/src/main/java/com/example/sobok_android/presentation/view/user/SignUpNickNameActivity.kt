c8a9501102dcbf3e8597df808269785e840ed84c
garden <0401ah@gmail.com>
2022-01-20 18:00:21 +0900

    [UI] 로그인&회원가입 레이아웃

--- app/src/main/java/com/example/sobok_android/presentation/view/user/SignUpNickNameActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/user/SignUpNickNameActivity.kt
@@ -3,31 +3,32 @@ package com.example.sobok_android.presentation.view.user
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.sobok_android.R
 import com.example.sobok_android.databinding.ActivitySignInBinding
+import com.example.sobok_android.databinding.ActivitySignUpNickNameBinding
 import com.example.sobok_android.presentation.base.BindingActivity
-import com.example.sobok_android.presentation.view.MainActivity
 
-class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
+class SignUpNickNameActivity : BindingActivity<ActivitySignUpNickNameBinding>(R.layout.activity_sign_up_nick_name) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        btnSignUpEvent()
-        btnSignInEvent()
+        btnBackSignUpEvent()
+        btnSignUpFinishEvent()
     }
 
-    private fun btnSignUpEvent() {
-        binding.tvSignUp.setOnClickListener {
+    private fun btnBackSignUpEvent() {
+        binding.ivBackSignUp.setOnClickListener {
+            Log.d("btnBackSignUpEvetn", "success")
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
-
+            finish()
         }
     }
 
-    private fun btnSignInEvent() {
-        binding.btnSignIn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+    private fun btnSignUpFinishEvent() {
+        binding.btnSignUpFinish.setOnClickListener {
+            val intent = Intent(this, SignUpWelcomeActivity::class.java)
             startActivity(intent)
-
         }
     }
 }
