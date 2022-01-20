d8fa3624e3e207824ee8a19ec0301ab4e493e74e
garden <0401ah@gmail.com>
2022-01-21 07:29:41 +0900

    [UI] #36 - ClearEditText 적용중(미완성)

--- app/src/main/java/com/example/sobok_android/presentation/view/user/SignInActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/user/SignInActivity.kt
@@ -1,8 +1,11 @@
 package com.example.sobok_android.presentation.view.user
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.ViewModel
 import com.example.sobok_android.R
 import com.example.sobok_android.databinding.ActivitySignInBinding
@@ -46,4 +49,33 @@ class SignInActivity : BindingActivity<ActivitySignInBinding>(R.layout.activity_
             startActivity(intent)
         }
     }
+
+//    private fun setDeleteClickListener() {
+//        binding.ivTextCloseEmail.setOnClickListener {
+//            binding.etSignInEmail.text = null
+//        }
+//        binding.ivTextClosePw.setOnClickListener {
+//            binding.etSignInPw.text = null
+//        }
+//    }
+//
+//    private fun hideKeyBoard() {
+//        val inputMethodManager: InputMethodManager =
+//            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        inputMethodManager.hideSoftInputFromWindow(
+//            binding.etSignInEmail.windowToken,
+//            0
+//        )
+//    }
+//
+//    private fun setEtShareRequestOnFocusChangeListener() {
+//        binding.etSignInEmail.setOnFocusChangeListener { view, hasFocus ->
+//            if (hasFocus) {
+//                binding.isEditable = true
+//            } else {
+//                binding.isEditable = false
+//                hideKeyBoard()
+//            }
+//        }
+//    }
 }
