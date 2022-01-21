e9355e5481a74565ffe06dd856a1669ac905cd32
chaeyeong <shimcy2010@gmail.com>
2022-01-21 22:48:02 +0900

    Merge branch 'feature/24-pill-add-one-page' into develop

--- app/src/main/java/com/example/sobok_android/presentation/di/DataSourceModule.kt
+++ app/src/main/java/com/example/sobok_android/presentation/di/DataSourceModule.kt
@@ -5,17 +5,21 @@ import com.example.sobok_android.data.datasource.notice.NoticeRemoteDataSource
 import com.example.sobok_android.data.datasource.calendar.CalendarDataSource
 import com.example.sobok_android.data.datasource.calendar.CalendarRemoteDataSource
 import com.example.sobok_android.data.datasource.pill.pilladd.PillAddDataSource
-import com.example.sobok_android.data.datasource.pill.pilladd.PillAddLocalDataSource
+import com.example.sobok_android.data.datasource.pill.pilladd.PillAddRemoteDataSource
 import com.example.sobok_android.data.datasource.share.request.ShareRequestDataSource
 import com.example.sobok_android.data.datasource.share.request.ShareRequestRemoteDataSource
 import org.koin.dsl.module
 
 val dataSourceModule = module {
+<<<<<<< HEAD
 
     single<NoticeDataSource> {
         NoticeRemoteDataSource(get())
     }
     single<PillAddDataSource> { PillAddLocalDataSource() }
+=======
+    single<PillAddDataSource> { PillAddRemoteDataSource(get()) }
+>>>>>>> 24-pill-add-one-page
     single<ShareRequestDataSource> { ShareRequestRemoteDataSource(get()) }
     single<CalendarDataSource> { CalendarRemoteDataSource(get()) }
 }
