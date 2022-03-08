8273e7dce8716a2518e6543c2d3b410afa773610
chaeyeong <shimcy2010@gmail.com>
2022-03-08 22:27:21 +0900

    [UI] 약추가 바뀐 뷰

---
+++ app/src/main/java/com/example/sobok_android/presentation/view/pill/add/PillAddFormNameFragment.kt
@@ -0,0 +1,12 @@
+package com.example.sobok_android.presentation.view.pill.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sobok_android.R
+
+class PillAddFormNameFragment : Fragment() {
+
+}
