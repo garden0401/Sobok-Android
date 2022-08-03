d755de8002b0b03245aeafc983e7f13186883876
onpyeong <gsh447114@gmail.com>
2022-08-04 02:56:34 +0900

    [FIX] 공유요청 엠티뷰 연결

--- app/src/main/java/com/example/sobok_android/presentation/view/share/ShareFragment.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/share/ShareFragment.kt
@@ -1,16 +1,11 @@
 package com.example.sobok_android.presentation.view.share
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
 import com.example.sobok_android.R
 import com.example.sobok_android.databinding.FragmentShareBinding
 import com.example.sobok_android.presentation.base.BindingFragment
 import com.example.sobok_android.presentation.view.home.HomeFragment
-import com.example.sobok_android.presentation.view.share.request.ShareRequestActivity
 import com.example.sobok_android.presentation.view.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
@@ -19,7 +14,6 @@ class ShareFragment : BindingFragment<FragmentShareBinding>(R.layout.fragment_sh
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setCalendarFragment()
         observeGroupData()
         setBtnPlusClickListener()
 
@@ -32,64 +26,72 @@ class ShareFragment : BindingFragment<FragmentShareBinding>(R.layout.fragment_sh
 
     private fun observeGroupData() {
         mainViewModel.groupData.observe(viewLifecycleOwner) {
-            Log.d("GroupData-", "${it.size}")
             with(binding) {
-                if (it.isNotEmpty()) {
-                    //TODO: member이름 한글자인 경우도 가능하도록 수정
-                    tvMemberOne.text = it[0].memberName.substring(0, 2)
-                    tvMemberOne.isSelected = true
-                    mainViewModel.setSelectedMemberName(tvMemberOne.text.toString())
-                    tvMemberOne.setTextAppearance(R.style.MemberSelectTextStyle)
-                    tvMemberOne.setOnClickListener {
-                        if (!tvMemberOne.isSelected) {
-                            tvMemberOne.isSelected = true
-                            mainViewModel.setSelectedMemberName(tvMemberOne.text.toString())
-                            setAllMemberNotSelected()
-                            tvMemberOne.setTextAppearance(R.style.MemberSelectTextStyle)
+                if (it.isEmpty()) {
+                    isGroupEmpty = true
+                    //TODO: 함수로 정리
+                    childFragmentManager.beginTransaction()
+                        .add(R.id.fcv_share_calendar, ShareEmptyFragment()).commit()
+                } else {
+                    isGroupEmpty = false
+                    setCalendarFragment()
+                    if (it.isNotEmpty()) {
+                        //TODO: member이름 한글자인 경우도 가능하도록 수정
+                        tvMemberOne.text = it[0].memberName.substring(0, 2)
+                        tvMemberOne.isSelected = true
+                        mainViewModel.setSelectedMemberName(tvMemberOne.text.toString())
+                        tvMemberOne.setTextAppearance(R.style.MemberSelectTextStyle)
+                        tvMemberOne.setOnClickListener {
+                            if (!tvMemberOne.isSelected) {
+                                tvMemberOne.isSelected = true
+                                mainViewModel.setSelectedMemberName(tvMemberOne.text.toString())
+                                setAllMemberNotSelected()
+                                tvMemberOne.setTextAppearance(R.style.MemberSelectTextStyle)
+                            }
                         }
                     }
-                }
-                if (it.size > 1) {
-                    tvMemberTwo.text = it[1].memberName.substring(0, 2)
-                    tvMemberTwo.setOnClickListener {
-                        if (!tvMemberTwo.isSelected) {
-                            tvMemberTwo.isSelected = true
-                            mainViewModel.setSelectedMemberName(tvMemberTwo.text.toString())
-                            setAllMemberNotSelected()
-                            tvMemberTwo.setTextAppearance(R.style.MemberSelectTextStyle)
+                    if (it.size > 1) {
+                        tvMemberTwo.text = it[1].memberName.substring(0, 2)
+                        tvMemberTwo.setOnClickListener {
+                            if (!tvMemberTwo.isSelected) {
+                                tvMemberTwo.isSelected = true
+                                mainViewModel.setSelectedMemberName(tvMemberTwo.text.toString())
+                                setAllMemberNotSelected()
+                                tvMemberTwo.setTextAppearance(R.style.MemberSelectTextStyle)
+                            }
                         }
                     }
-                }
-                if (it.size > 2) {
-                    tvMemberThree.text = it[2].memberName.substring(0, 2)
-                    tvMemberThree.setOnClickListener {
-                        if (!tvMemberThree.isSelected) {
-                            tvMemberThree.isSelected = true
-                            mainViewModel.setSelectedMemberName(tvMemberThree.text.toString())
-                            setAllMemberNotSelected()
-                            tvMemberThree.setTextAppearance(R.style.MemberSelectTextStyle)
+                    if (it.size > 2) {
+                        tvMemberThree.text = it[2].memberName.substring(0, 2)
+                        tvMemberThree.setOnClickListener {
+                            if (!tvMemberThree.isSelected) {
+                                tvMemberThree.isSelected = true
+                                mainViewModel.setSelectedMemberName(tvMemberThree.text.toString())
+                                setAllMemberNotSelected()
+                                tvMemberThree.setTextAppearance(R.style.MemberSelectTextStyle)
+                            }
                         }
                     }
-                }
-                if (it.size > 3) {
-                    tvMemberFour.text = it[3].memberName.substring(0, 2)
-                    tvMemberFour.setOnClickListener {
-                        if (!tvMemberFour.isSelected) {
-                            tvMemberFour.isSelected = true
-                            mainViewModel.setSelectedMemberName(tvMemberFour.text.toString())
-                            setAllMemberNotSelected()
-                            tvMemberFour.setTextAppearance(R.style.MemberSelectTextStyle)
+                    if (it.size > 3) {
+                        tvMemberFour.text = it[3].memberName.substring(0, 2)
+                        tvMemberFour.setOnClickListener {
+                            if (!tvMemberFour.isSelected) {
+                                tvMemberFour.isSelected = true
+                                mainViewModel.setSelectedMemberName(tvMemberFour.text.toString())
+                                setAllMemberNotSelected()
+                                tvMemberFour.setTextAppearance(R.style.MemberSelectTextStyle)
+                            }
                         }
                     }
-                }
-                if(it.size > 4) {
-                    tvMemberFive.text = it[4].memberName.substring(0, 2)
-                    tvMemberFive.setOnClickListener {
-                        if (!tvMemberFive.isSelected) {
-                            tvMemberFive.isSelected = true
-                            mainViewModel.setSelectedMemberName(tvMemberFive.text.toString())
-                            setAllMemberNotSelected()
-                            tvMemberFive.setTextAppearance(R.style.MemberSelectTextStyle)
+                    if (it.size > 4) {
+                        tvMemberFive.text = it[4].memberName.substring(0, 2)
+                        tvMemberFive.setOnClickListener {
+                            if (!tvMemberFive.isSelected) {
+                                tvMemberFive.isSelected = true
+                                mainViewModel.setSelectedMemberName(tvMemberFive.text.toString())
+                                setAllMemberNotSelected()
+                                tvMemberFive.setTextAppearance(R.style.MemberSelectTextStyle)
+                            }
                         }
                     }
                 }
@@ -115,7 +117,7 @@ class ShareFragment : BindingFragment<FragmentShareBinding>(R.layout.fragment_sh
     private fun setBtnPlusClickListener() {
         binding.ivShareRequestPlus.setOnClickListener {
             //TODO: 친구 숫자 세서 5명 넘으면 팝업 띄우기
-            mainViewModel.setIsShareRequestClick(true)
+            //mainViewModel.setIsShareRequestClick(true)
         }
     }
 
