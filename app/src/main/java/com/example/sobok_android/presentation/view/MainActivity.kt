f96d6148295e6450e4dc1d30d3c935352c650974
onpyeong <gsh447114@gmail.com>
2022-08-04 02:55:29 +0900

    [DEL] 필요없는 코드 삭제

--- app/src/main/java/com/example/sobok_android/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.sobok_android.presentation.view
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.sobok_android.R
 import com.example.sobok_android.databinding.ActivityMainBinding
@@ -15,7 +13,6 @@ import com.example.sobok_android.presentation.view.notice.NoticeFragment
 import com.example.sobok_android.presentation.view.pill.add.PillAddActivity
 import com.example.sobok_android.presentation.view.pill.add.PillAddBottomSheetFragment
 import com.example.sobok_android.presentation.view.share.ShareFragment
-import com.example.sobok_android.presentation.view.share.request.ShareRequestActivity
 import com.example.sobok_android.presentation.view.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -34,8 +31,6 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         initHomeStickerBottomSheet()
         setNavigation()
         observeIsStickerClickEvent()
-        observeIsShareRequest()
-        observeIsShareRequestClick()
         observePillAddNavigateData()
     }
 
@@ -91,12 +86,6 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         }
     }
 
-    private fun observeIsShareRequest() {
-        mainViewModel.isShareRequest.observe(this) {
-
-        }
-    }
-
     private fun observePillAddNavigateData() {
         mainViewModel.pillAddNavigateData.observe(this) {
             val intent = Intent(this, PillAddActivity::class.java)
@@ -109,32 +98,4 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
             startActivity(intent)
         }
     }
-
-    private val shareRequestActivityLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {
-        if (it.resultCode == RESULT_OK) {
-            //TODO : 팝업
-            if (mainViewModel.isShareRequest.value == true) {
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("캘린더 공유 요청을 보냈어요!")
-                    .setMessage(
-                        "상대방이 요청을 수락하면,\n" +
-                                "상대방의 캘린더를 볼 수 있어요"
-                    )
-                    .setNegativeButton("확인") { dialog, id ->
-                    }
-                builder.show()
-            }
-        }
-    }
-
-    private fun observeIsShareRequestClick() {
-        mainViewModel.isShareRequestClick.observe(this) {
-            if (it) {
-                shareRequestActivityLauncher.launch(Intent(this, ShareRequestActivity::class.java))
-            }
-        }
-    }
-
 }
