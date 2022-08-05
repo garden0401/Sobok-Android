d23c9fbfa35e22fad8ab47f56bd8d4d7e5eb1157
garden <0401ah@gmail.com>
2022-08-05 16:01:54 +0900

    [CHORE] #62 - 알림뷰 ui 및 로직 수정
    - goneMargin값 수정
    - section Data에 따른 분기처리 수정

--- app/src/main/java/com/example/sobok_android/presentation/view/noticeSecond/NoticeFragment2.kt
+++ app/src/main/java/com/example/sobok_android/presentation/view/noticeSecond/NoticeFragment2.kt
@@ -29,10 +29,10 @@ class NoticeFragment2 : BindingFragment<FragmentNotice2Binding>(R.layout.fragmen
                     NoticeData.Data.Info(44, "pill", "waiting", true, "오후 12:35", "cryon", "정관장", 453)
                 )),
                 NoticeData(200, true, "알림리스트 조회 성공", NoticeData.Data(
-                    NoticeData.Data.Info(45, "calendar", "refuse", true, "오후 11:35", "garden", "비타민D", 453)
+                    NoticeData.Data.Info(45, "calendar", "refuse", true, "오후 11:35", "garden", "", 453)
                 )),
                 NoticeData(200, true, "알림리스트 조회 성공", NoticeData.Data(
-                    NoticeData.Data.Info(45, "calendar", "accept", true, "오후 11:37", "garden", "비타민D", 453)
+                    NoticeData.Data.Info(45, "calendar", "accept", true, "오후 11:37", "garden", "", 453)
                 )),
                 NoticeData(200, true, "알림리스트 조회 성공", NoticeData.Data(
                     NoticeData.Data.Info(46, "pill", "accept", true, "오후 12:30", "abc", "루테인", 453)
@@ -41,7 +41,7 @@ class NoticeFragment2 : BindingFragment<FragmentNotice2Binding>(R.layout.fragmen
                     NoticeData.Data.Info(47, "pill", "accept", true, "오후 12:35", "lou", "임상실험약", 453)
                 )),
                 NoticeData(200, true, "알림리스트 조회 성공", NoticeData.Data(
-                    NoticeData.Data.Info(48, "calendar", "waiting", true, "오후 12:37", "yayayay", "비타민C", 453)
+                    NoticeData.Data.Info(48, "calendar", "waiting", true, "오후 12:37", "yayayay", "", 453)
                 )),
                 NoticeData(200, true, "알림리스트 조회 성공", NoticeData.Data(
                     NoticeData.Data.Info(49, "pill", "refuse", true, "오후 12:35", "choi", "홍상", 453)
