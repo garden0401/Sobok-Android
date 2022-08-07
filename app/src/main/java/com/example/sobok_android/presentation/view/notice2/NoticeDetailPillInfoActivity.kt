aed19ba295e581f10b065d2ed50c4479587d990b
garden <0401ah@gmail.com>
2022-08-07 19:22:30 +0900

    [CHORE] #62 - drawable 파일 추가

---
+++ app/src/main/java/com/example/sobok_android/presentation/view/notice2/NoticeDetailPillInfoActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sobok_android.presentation.view.notice2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sobok_android.R
+import com.example.sobok_android.databinding.ActivityNoticeDetailPillInfoBinding
+import com.example.sobok_android.presentation.base.BindingActivity
+
+class NoticeDetailPillInfoActivity : BindingActivity<ActivityNoticeDetailPillInfoBinding>(R.layout.activity_notice_detail_pill_info) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
